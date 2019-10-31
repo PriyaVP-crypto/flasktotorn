5dd5fce8184d768c609949cc97ebe65ac63e08ea
PriyaVP-crypto <56911834+PriyaVP-crypto@users.noreply.github.com>
2019-10-31 12:26:25 +0530

    Update wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -1,35 +1,9 @@
-import os
-import json
-import requests
-import tornado.web
-import tornado.ioloop
-import tornado.autoreload
+from flask import Flask
+application = Flask(__name__)
 
-# On IBM Cloud Cloud Foundry, get the port number from the environment variable PORT
-# When running this app on the local machine, default the port to 8000
-port = int(os.getenv('PORT', 8000))
+@application.route("/")
+def hello():
+    return "Hello World! Version 3 code changed"
 
-class landingPage(tornado.web.RequestHandler):
-    def get(self):
-        self.render("indexx.html")
-
-        
 if __name__ == "__main__":
-    app = tornado.web.Application([
-        (r"/", landingPage),
-        (r"/register", basicRequestHandler),
-        (r"/regrequ", regRequ),
-        (r"/deregister", basicDeRequestHandler),
-        (r"/deregrequ", deRegRequ),
-        (r"/paymentauth", basicPayHandler),
-        (r"/payrequ", payRequ),
-        (r"/paymentrevauth", basicRevHandler),
-        (r"/revRequ", revRequ),
-    ])
-
-    app.listen(port)
-    # TODO remove in prod
-    tornado.autoreload.start()
-    print("I'm listening on port specified")
-    tornado.ioloop.IOLoop.current().start()
-
+    application.run()
