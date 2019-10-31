b77089e381823f3679e8d905c49f27dc18e4a727
PriyaVP-crypto <56911834+PriyaVP-crypto@users.noreply.github.com>
2019-10-31 12:47:03 +0530

    Update wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -1,9 +1,34 @@
-from flask import Flask
-application = Flask(__name__)
+import os
+import json
+import requests
+import tornado.web
+import tornado.ioloop
+import tornado.autoreload
 
-@application.route("/")
-def hello():
-    return "Enjoy new code"
+from flask import Flask
+port = int(os.getenv('PORT', 8000))
+
+class landingPage(tornado.web.RequestHandler):
+    def get(self):
+        self.render("indexx.html")
+
 
 if __name__ == "__main__":
-    application.run()
+    app = tornado.web.Application([
+        (r"/", landingPage),
+        (r"/register", basicRequestHandler),
+        (r"/regrequ", regRequ),
+        (r"/deregister", basicDeRequestHandler),
+        (r"/deregrequ", deRegRequ),
+        (r"/paymentauth", basicPayHandler),
+        (r"/payrequ", payRequ),
+        (r"/paymentrevauth", basicRevHandler),
+        (r"/revRequ", revRequ),
+    ])
+
+    app.listen(port)
+    # TODO remove in prod
+    tornado.autoreload.start()
+    print("I'm listening on port specified")
+    tornado.ioloop.IOLoop.current().start()
+
