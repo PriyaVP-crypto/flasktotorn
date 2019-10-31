7ac5eef62e9c484c8f3af8254844bbd4281918c7
PriyaVP-crypto <56911834+PriyaVP-crypto@users.noreply.github.com>
2019-10-31 14:03:30 +0530

    Update app.py

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ port = int(os.getenv('PORT', 8000))
 
 class landingPage(tornado.web.RequestHandler):
     def get(self):
-        self.render("indexx.html")
+        self.render("static/indexx.html")
 if __name__ == "__main__":
     app = tornado.web.Application([
         (r"/", landingPage),
